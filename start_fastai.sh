5121a4e1ac15747d54caa17d9d9ce4406a0d8cfc
beanzsoft <beanzsoftai@gmail.com>
2022-03-21 20:43:28 +0900

    add to installation nbdev

--- start_fastai.sh
+++ start_fastai.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-nvidia-docker run -it --rm -p 8888:8888 -p 6006:6006 -p 7001:7001 -h beanzsoft \
+nvidia-docker run -it --rm -p 4000:4000 -p 8888:8888 -p 6006:6006 -p 7001:7001 -h beanzsoft \
  --name beanzsoft -v ~/:/home/beanzsoft/data \
 --ipc=host beanzsoft/research:latest jupyter notebook \
 --no-browser --ip=0.0.0.0 --allow-root \
